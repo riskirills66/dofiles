8f7571ff629c79ccc59a9eb9d1697b06afdcb457
riskirills66 <riskirills66@gmail.com>
2025-12-26 11:24:31 +0700

    colorscheme no transparent nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,26 +1,26 @@
-vim.cmd([[
-  augroup TransparentBackground
-    autocmd!
-    autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
-    autocmd ColorScheme * highlight NormalNC ctermbg=none guibg=none
-    autocmd ColorScheme * highlight SignColumn ctermbg=none guibg=none
-    autocmd ColorScheme * highlight VertSplit ctermbg=none guibg=none
-    autocmd ColorScheme * highlight StatusLine ctermbg=none guibg=none
-  augroup END
-]])
-
-vim.cmd([[
-  augroup TransparentFloat
-    autocmd!
-    autocmd ColorScheme * highlight NormalFloat ctermbg=none guibg=none
-    autocmd ColorScheme * highlight FloatBorder ctermbg=none guibg=none
-    autocmd ColorScheme * highlight Pmenu ctermbg=none guibg=none
-    autocmd ColorScheme * highlight PmenuSel ctermbg=none guibg=none
-    autocmd ColorScheme * highlight TelescopeNormal ctermbg=none guibg=none
-    autocmd ColorScheme * highlight TelescopeBorder ctermbg=none guibg=none
-  augroup END
-]])
-
+-- vim.cmd([[
+--   augroup TransparentBackground
+--     autocmd!
+--     autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight NormalNC ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight SignColumn ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight VertSplit ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight StatusLine ctermbg=none guibg=none
+--   augroup END
+-- ]])
+--
+-- vim.cmd([[
+--   augroup TransparentFloat
+--     autocmd!
+--     autocmd ColorScheme * highlight NormalFloat ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight FloatBorder ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight Pmenu ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight PmenuSel ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight TelescopeNormal ctermbg=none guibg=none
+--     autocmd ColorScheme * highlight TelescopeBorder ctermbg=none guibg=none
+--   augroup END
+-- ]])
+--
 -- Enable autoread to automatically reload files edited outside Neovim
 vim.o.autoread = true
 
@@ -43,34 +43,37 @@ local function setup_file_watcher(bufnr)
   if file_watchers[bufnr] then
     return -- Already watching this buffer
   end
-  
+
   local filename = vim.api.nvim_buf_get_name(bufnr)
   if filename == "" or vim.fn.isdirectory(filename) == 1 then
     return -- Skip empty buffers and directories
   end
-  
+
   local timer = vim.loop.new_timer()
   local last_mtime = vim.fn.getftime(filename)
-  
+
   timer:start(1000, 1000, function()
     local current_mtime = vim.fn.getftime(filename)
     if current_mtime ~= last_mtime and current_mtime > 0 then
       last_mtime = current_mtime
       vim.schedule(function()
-        if vim.api.nvim_buf_is_valid(bufnr) and not vim.api.nvim_buf_get_option(bufnr, 'modified') then
+        if vim.api.nvim_buf_is_valid(bufnr) and not vim.api.nvim_buf_get_option(bufnr, "modified") then
           vim.api.nvim_buf_call(bufnr, function()
-            vim.cmd('silent! edit!')
-            vim.notify("File '" .. vim.fn.fnamemodify(filename, ':t') .. "' was modified externally and reloaded", vim.log.levels.INFO)
+            vim.cmd("silent! edit!")
+            vim.notify(
+              "File '" .. vim.fn.fnamemodify(filename, ":t") .. "' was modified externally and reloaded",
+              vim.log.levels.INFO
+            )
           end)
         end
       end)
     end
   end)
-  
+
   file_watchers[bufnr] = timer
-  
+
   -- Clean up when buffer is deleted
-  vim.api.nvim_create_autocmd('BufDelete', {
+  vim.api.nvim_create_autocmd("BufDelete", {
     buffer = bufnr,
     callback = function()
       if file_watchers[bufnr] then
@@ -83,15 +86,15 @@ local function setup_file_watcher(bufnr)
 end
 
 -- Start watching when a buffer is loaded
-vim.api.nvim_create_autocmd('BufReadPost', {
+vim.api.nvim_create_autocmd("BufReadPost", {
   callback = function(args)
     setup_file_watcher(args.buf)
-  end
+  end,
 })
 
 -- Start watching for new buffers
-vim.api.nvim_create_autocmd('BufNewFile', {
+vim.api.nvim_create_autocmd("BufNewFile", {
   callback = function(args)
     setup_file_watcher(args.buf)
-  end
+  end,
 })
