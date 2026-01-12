250ea9cddff7e246e69421064ee72b6771c02e14
riskirills66 <riskirills66@gmail.com>
2026-01-12 20:48:50 +0700

    modernize hyprland look

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,25 +1,25 @@
--- vim.cmd([[
---   augroup TransparentBackground
---     autocmd!
---     autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
---     autocmd ColorScheme * highlight NormalNC ctermbg=none guibg=none
---     autocmd ColorScheme * highlight SignColumn ctermbg=none guibg=none
---     autocmd ColorScheme * highlight VertSplit ctermbg=none guibg=none
---     autocmd ColorScheme * highlight StatusLine ctermbg=none guibg=none
---   augroup END
--- ]])
---
--- vim.cmd([[
---   augroup TransparentFloat
---     autocmd!
---     autocmd ColorScheme * highlight NormalFloat ctermbg=none guibg=none
---     autocmd ColorScheme * highlight FloatBorder ctermbg=none guibg=none
---     autocmd ColorScheme * highlight Pmenu ctermbg=none guibg=none
---     autocmd ColorScheme * highlight PmenuSel ctermbg=none guibg=none
---     autocmd ColorScheme * highlight TelescopeNormal ctermbg=none guibg=none
---     autocmd ColorScheme * highlight TelescopeBorder ctermbg=none guibg=none
---   augroup END
--- ]])
+vim.cmd([[
+  augroup TransparentBackground
+    autocmd!
+    autocmd ColorScheme * highlight Normal ctermbg=none guibg=none
+    autocmd ColorScheme * highlight NormalNC ctermbg=none guibg=none
+    autocmd ColorScheme * highlight SignColumn ctermbg=none guibg=none
+    autocmd ColorScheme * highlight VertSplit ctermbg=none guibg=none
+    autocmd ColorScheme * highlight StatusLine ctermbg=none guibg=none
+  augroup END
+]])
+
+vim.cmd([[
+  augroup TransparentFloat
+    autocmd!
+    autocmd ColorScheme * highlight NormalFloat ctermbg=none guibg=none
+    autocmd ColorScheme * highlight FloatBorder ctermbg=none guibg=none
+    autocmd ColorScheme * highlight Pmenu ctermbg=none guibg=none
+    autocmd ColorScheme * highlight PmenuSel ctermbg=none guibg=none
+    autocmd ColorScheme * highlight TelescopeNormal ctermbg=none guibg=none
+    autocmd ColorScheme * highlight TelescopeBorder ctermbg=none guibg=none
+  augroup END
+]])
 --
 -- Enable autoread to automatically reload files edited outside Neovim
 vim.o.autoread = true
